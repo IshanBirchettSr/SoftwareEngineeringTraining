656231113aef479537928a8db737bd47ebed4d7a
Ishan Dan Birchett, Sr <ibirchettsr@gmail.com>
2020-05-14 21:40:14 -0400

    Methods, Parameters and if
    Took Charlenes home work and extended it to have more examples of methods with parameters and introducted if <true> then , false Else.

--- homework/VarExercise.java
+++ homework/VarExercise.java
@@ -9,6 +9,18 @@ public class VarExercise {
     dv.setTown(aTown);
     byte myTown = dv.getTown();
     System.out.printf("This is my town: %c\n", myTown);
-  }
+    
+      short ballParks = 1000;
+      float costOfSmartWater = 3.75f;
+      dv.setOuttings(ballParks, costOfSmartWater);
+      int howManyHomes = 20000;
+      long population = 34000;
+      dv.neighbors(howManyHomes, population);
+      boolean lightsOn = true;
+      dv.isStreetLightsOn(lightsOn);
+      lightsOn = false;
+      dv.isStreetLightsOn(lightsOn);
+      dv.isThereChange(5.45f, 5.45f);
+  }   
 }
     
