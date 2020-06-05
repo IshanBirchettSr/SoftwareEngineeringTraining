d4f647119dcc21576be55a305e610665c5461a6c
allmajohnson <65092128+allmajohnson@users.noreply.github.com>
2020-06-04 17:32:39 -0700

    WhosNameWhileLooping
    Thank you for your time!

---
+++ homework/aj-homework/WhosName2.java
@@ -0,0 +1,6 @@
+public class WhosName2 {
+    public static void main(String[] args) {
+    WhosLineIsItWhileLooping isItYourName = new WhosLineIsItWhileLooping();
+    isItYourName.knowYourName();
+  } 
+}
