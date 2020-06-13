c93d00aefa2809b23bfaf354f45bc1107ecbb6c7
Roxanne L. Earnest <64979895+Malachi236@users.noreply.github.com>
2020-06-13 18:59:48 -0400

    Added new Department
    Added the Electronics Department

---
+++ projects/src/main/java/com/usi/src/electronics/ElectronicsDept.java
@@ -0,0 +1,32 @@
+/**
+ * Property of Urban Survival Initiative
+ * Copyright (c) Notice 2020 - All rights reserved
+ * 
+ * This software is owned in whole by the Urban Survival Initiative.  It's intent purpose
+ * is for training individuals to be software engineers.
+ * 
+ */
+package electronics;
+
+/**
+ * @author malac
+ *
+ */
+public class ElectronicsDept {
+
+	/**
+	 * 
+	 */
+	public ElectronicsDept() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
