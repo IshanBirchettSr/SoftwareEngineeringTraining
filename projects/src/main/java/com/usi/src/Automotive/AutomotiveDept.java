3ece7326aae4ab8d06a8b005681d7f0ba2fc6be1
Roxanne L. Earnest <64979895+Malachi236@users.noreply.github.com>
2020-06-20 12:05:08 -0400

    REarnest created packages
    created
    Automotive Dept
    PerscriptionEyeware Dept
    Shoe Dept

---
+++ projects/src/main/java/com/usi/src/Automotive/AutomotiveDept.java
@@ -0,0 +1,14 @@
+package Automotive;
+
+import util.Department;
+
+	public class AutomotiveDept {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
---
+++ projects/src/main/java/com/usi/src/PerscriptionEyeware/PescriptionEyeware.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package PerscriptionEyeware;
+import util.Department;
+/**
+ * @author malac
+ *
+ */
+public class PescriptionEyeware {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
