b4f81f1e84e52fa1a8c1d470135333041ef918c7
allmajohnson <65092128+allmajohnson@users.noreply.github.com>
2020-06-20 10:42:39 -0700

    fetch
    fetch

---
+++ projects/src/main/java/com/usi/src/linen/LinenDept.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package linen;
+
+import util.Department;
+
+/**
+ * @author Allma M. Johnson
+ *
+ */
+public class LinenDept extends Department {
+	
+	/**
+	 * 
+	 */
+	public LinenDept() {
+		
+	}
+
+}
