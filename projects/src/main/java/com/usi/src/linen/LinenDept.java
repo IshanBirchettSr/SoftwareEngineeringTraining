72c32225e257ccf6ca0ece58d8f2f758eabe35e0
allmajohnson <65092128+allmajohnson@users.noreply.github.com>
2020-06-27 19:10:12 -0700

    UpdateLinenDept.Java
    Update Linen Dept

--- projects/src/main/java/com/usi/src/linen/LinenDept.java
+++ projects/src/main/java/com/usi/src/linen/LinenDept.java
@@ -3,12 +3,30 @@
  */
 package linen;
 
+import java.util.List;
+
+import util.DataCsvLoad;
 import util.Department;
+import util.StoreConstants;
 
 /**
  * @author Allma M. Johnson
  *
  */
 public class LinenDept extends Department {
+    String deptName = StoreConstants.LINEN_TRUCK;
+    List<String> autoRecords = null;
+
+    /**
+     * 
+     */
+    public LinenDept() {
+	DataCsvLoad unLoadTrucks = new DataCsvLoad();
+
+	unLoadTrucks.loadData(StoreConstants.LINEN_TRUCK);
+	List<String> loadedRecords = unLoadTrucks.getRecords();
+	this.setLoadedRecords(loadedRecords);
+	System.out.printf("%s Department open with %d products\n", deptName, loadedRecords.size());
+    }
 
 }
