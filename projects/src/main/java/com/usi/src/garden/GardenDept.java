e0283bad830b875dc46c55569b8d3401610511bc
Ishan Dan Birchett, Sr <ibirchettsr@gmail.com>
2020-06-30 19:13:57 -0400

    Garden Updates
    Resolving git issues

--- projects/src/main/java/com/usi/src/garden/GardenDept.java
+++ projects/src/main/java/com/usi/src/garden/GardenDept.java
@@ -8,13 +8,29 @@
  */
 package garden;
 
-import util.Department;
+import java.util.List;
 
+import util.DataCsvLoad;
+import util.Department;
+import util.StoreConstants;
 
 /**
  * @author chich
  *
  */
-public class GardenDept extends Department {
 
+public class GardenDept extends Department {
+    String deptName = StoreConstants.deptNames.GARDEN.name();
+
+    /**
+     * 
+     */
+    public GardenDept() {
+	DataCsvLoad unLoadTrucks = new DataCsvLoad();
+
+	unLoadTrucks.loadData(StoreConstants.GARDEN_TRUCK);
+	List<String> gardenRecords = unLoadTrucks.getRecords();
+	this.setLoadedRecords(gardenRecords);
+	System.out.printf("%s Department open with %d products\n", deptName, gardenRecords.size());
+    }
 }
---
+++ projects/src/main/java/com/usi/src/garden/GardenProd.java
@@ -0,0 +1,18 @@
+package garden;
+
+import util.Product;
+
+/**
+ * @author chich
+ *
+ */
+
+public class GardenProd extends Product {
+
+    @Override
+    public boolean loadProducts() {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+}
