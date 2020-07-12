8fa82b517ea4ae736d20fb6dcbbbd49ecbcb995c
Roxanne L. Earnest <64979895+Malachi236@users.noreply.github.com>
2020-07-12 15:06:07 -0400

    Added to StoreConstants
    Added TRUCKS Constants

--- projects/src/main/java/com/usi/src/util/StoreConstants.java
+++ projects/src/main/java/com/usi/src/util/StoreConstants.java
@@ -33,7 +33,11 @@ public class StoreConstants {
     public static final String BAKERY_TRUCK = APP_HOME + "/src/resources/Bakery.txt";
     public static final String BABY_ESSENTIALS_TRUCK = APP_HOME + "/src/resources/BabyEssentials.txt";
     public static final String CANNED_GOODS_TRUCKS = APP_HOME + "/src/resources/CannedGoods.txt";
-
+    public static final String DELI_TRUCK = APP_HOME + "/src/resources/Deli.txt";
+    public static final String DAIRY_TRUCK = APP_HOME + "/src/resources/Dairy.txt";  
+    public static final String FRAGRANCE_TRUCK = APP_HOME + "/src/resources/Fragrance.txt";
+    public static final String LUGGAGE_TRUCK = APP_HOME + "/src/resources/Luggage.txt";
+    public static final String PRODUCE_TRUCK = APP_HOME + "/src/resources/Produce.txt";
     private String dName;
 
     public static enum deptNames {
