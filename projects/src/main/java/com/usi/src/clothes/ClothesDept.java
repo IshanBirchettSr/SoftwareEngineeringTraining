2f3ea79f0b56c85835022a62c731fc562c0c4609
allmajohnson <65092128+allmajohnson@users.noreply.github.com>
2020-07-19 14:49:57 -0700

    Recommitting data files, depts and prods
    -alphabatized open dept list & store constants
    
    -resubmitted data, depts, & record files for:
     --bakery
    --bikes
    --cleaning supplies
    --clothes
    --deli
    --furniture
    --housewares
    --meat
    --pharmacy
    --plant base
    --seafood
    --stationary
    --toys
    
    -minor changes to
    --frozen foods
    --haircare
    
    *ran debugger clean for all depts

--- projects/src/main/java/com/usi/src/clothes/ClothesDept.java
+++ projects/src/main/java/com/usi/src/clothes/ClothesDept.java
@@ -1,20 +1,56 @@
 package clothes;
 
+import java.util.HashMap;
+import java.util.List;
+
+import util.DataCsvLoad;
 import util.Department;
+import util.ProdKeyGen;
+import util.StoreConstants;
+
+/**
+ * @author Allma M. Johnson
+ *
+ */
 
 public class ClothesDept extends Department {
+    String deptName = StoreConstants.deptNames.CLOTHES.name();
+    List<String> clothesRecords = null;
+    // HashMap<K, V> to hold ClothesProd objects.
+    HashMap<String, ClothesProd> clothesProducts;
 
     /**
-     * 
+     * Constructor
      */
     public ClothesDept() {
-	super();
-	// TODO Auto-generated constructor stub
+	// Record Load
+	DataCsvLoad unLoadTrucks = new DataCsvLoad();
+	unLoadTrucks.loadData(StoreConstants.CLOTHES_TRUCK);
+	clothesRecords = unLoadTrucks.getRecords();
+	this.setLoadedRecords(clothesRecords);
+	// System.out.printf("%s Department open with %d records\n", deptName,
+	// clothesRecords.size());
+
+	// Clothes Product Load
+	clothesProducts = new HashMap<String, ClothesProd>();
+	loadProducts();
     }
 
     @Override
     protected void loadProducts() {
-	// TODO Auto-generated method stub
-    }
+	// Load products
+	for (String record : clothesRecords) {
+	    ClothesProd ccp = new ClothesProd();
+	    boolean recordToProductSuccessful = ccp.recordToProduct(record);
 
+	    // If it fails to convert any field, don't add that object to autoProducts
+	    if (recordToProductSuccessful == true) {
+		String prodKey = ProdKeyGen.genKey(ccp);
+		clothesProducts.put(prodKey, ccp);
+	    }
+	}
+	System.out.printf("%s Department loaded %d (crates) and created %d types of products\n", deptName,
+		clothesRecords.size(), clothesProducts.size());
+
+    }
 }
