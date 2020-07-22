0cf2d356f834dd08338cda9dc2e19b4ed4970078
Charlene Cornelius <64879394+chichiwepe@users.noreply.github.com>
2020-07-22 16:13:50 -0400

    Update Receipt.java

--- projects/src/main/java/com/usi/src/smartcart/Receipt.java
+++ projects/src/main/java/com/usi/src/smartcart/Receipt.java
@@ -2,54 +2,14 @@
  * 
  */
 package smartcart;
-
-<<<<<<< Updated upstream
-
-
 import java.util.Scanner;
-
-import smartcart.ShoppingCart;
-=======
-import java.util.Scanner;
->>>>>>> Stashed changes
 import util.StorePrinter;
 
 /**
  * @author chich
  *
  */
-<<<<<<< Updated upstream
-public class Receipt  {
-	
-	Scanner scan = new Scanner(System.in);
-	Scanner in = null;
-	
-	
-	boolean yes = true;
-	StorePrinter storePrinter = null;
-		
-			public boolean transaction() {
-				storePrinter = new StorePrinter();
-				storePrinter.actionPerformed(null);
-				return false;
-			} 
-			
-			private boolean printReciept(boolean transaction) {
-			return false;
-			}
-			 {
-			    
-	    if (yes = true ) {
-	    	System.out.print ("Thank you for shopping today! Have a great day!");
-	    }
-	    
-	    else { 
-	    	System.out.printf("Here's your reciept, Have a great day!"); }
-	    
-             
-			}
-	
-=======
+
 public class Receipt extends StorePrinter {
 	 Scanner scan = null;
 	 
@@ -76,7 +36,7 @@ public class Receipt extends StorePrinter {
 		scan = new Scanner(System.in);
 		
 	}
->>>>>>> Stashed changes
+
 
 }
 
