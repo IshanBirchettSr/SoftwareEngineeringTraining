b3d1f76939718dd86f5ea703af10a1c4978ac576
Charlene Cornelius <chichiwepe@aol.com>
2020-07-23 18:00:55 -0400

    Update Receipt.java

--- projects/src/main/java/com/usi/src/smartcart/Receipt.java
+++ projects/src/main/java/com/usi/src/smartcart/Receipt.java
@@ -4,36 +4,38 @@
 package smartcart;
 
 import java.awt.event.ActionEvent;
+
 import java.util.Scanner;
+
+import util.StoreConstants;
 import util.StorePrinter;
 
 /**
  * @author chich
+
  *
  */
 
 public class Receipt extends StorePrinter {
-	 Scanner scan = null;
-	 static StorePrinter print = new StorePrinter();
 	 
+	static StorePrinter print = new StorePrinter();
+	Scanner scan = null;
+    
 	 
-	 
-	 public static void printReceipt(ActionEvent e) {
-		 print.actionPerformed(e);
-		 System.out.println("Here is your reciept. Thank you for shopping at the Super Store today!");
+	  
+	 public static void printReceipt(ActionEvent e)  {
+		
 		 
+		 System.out.printf("Here is your reciept. Thank you for shopping at the %s\n today!", StoreConstants.STORE_NAME);
+		 print.actionPerformed(e);
 		 
 	 }
-	 
-	 
-	 
-	 
 	 
 	 
 	 public static void emailReceipt() {
 		 
 		 
-		 System.out.print("Your reciept will be emailed to you. Thank you for shopping at the Super Store today!" );
+		 System.out.printf("Your reciept will be emailed to you. Thank you for shopping at the %s\\n today!", StoreConstants.STORE_NAME);
 	 }
 
 	/**
