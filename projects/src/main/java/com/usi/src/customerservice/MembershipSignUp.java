a3d33a45a6a1e869a9fe3910e6fdf0d27aaf45a0
Roxanne L. Earnest <64979895+Malachi236@users.noreply.github.com>
2020-07-25 16:42:56 -0400

    Building code to Membership Greeting Customer
    Beginning stages of design and code for
    Membershipcard
    Membership signup
    Greeting

--- projects/src/main/java/com/usi/src/customerservice/MembershipSignUp.java
+++ projects/src/main/java/com/usi/src/customerservice/MembershipSignUp.java
@@ -19,27 +19,51 @@ import util.StoreConstants;
  */
 public class MembershipSignUp {
 
-	Scanner in = null;
+    Scanner in = null;
+    // put all data members declarations here
+    int phoneNumber = 0;
 
-	/**
-	 * 
-	 */
-	public MembershipSignUp(Scanner inComing) {
-		super();
-		in = inComing;
-	}
+    /**
+     
+     */
+    public MembershipSignUp(Scanner inComing) {
+	in = inComing;
+    }
 
-	public String membershipApplication() {
-		System.out.println("Please enter your First Name: ");
-		String firstName = in.next();
-		System.out.println("Please enter your Middle Name : ");
-		String middleName = in.next();
-		System.out.println("Please enter your Last Name: ");
-		String lastName = in.next();
-		String membershipId = String.format(
-				"Customer: %s %s %s Membership ID is: %s, enjoy your %d%% discount starting today!\n", firstName,
-				middleName, lastName, UUID.randomUUID().toString(), StoreConstants.TODAYS_MEMBER_DISCOUNT);
-		return membershipId;
-	}
+    /**
+     * @return the phoneNumber
+     */
+    public int getPhoneNumber() {
+	return phoneNumber;
+    }
+
+    /**
+     * @param mobilenumber the phoneNumber to set
+     */
+    public void setPhoneNumber(int phoneNumber) {
+	this.phoneNumber = phoneNumber;
+    }
+
+    public String membershipApplication() {
+	System.out.println("Please enter your First Name: ");
+	String firstName = in.next();
+	System.out.println("Please enter your Middle Initial : ");
+	String middleName = in.next();
+	System.out.println("Please enter your Last Name: ");
+	String lastName = in.next();
+	System.out.println("Please enter your email address: ");
+	String emailaddress = in.next();
+	System.out.println("Please enter your mailing address: ");
+	String mailingaddress = in.next();
+	System.out.println("Please enter your phone number: ");
+	phoneNumber = in.nextInt();
+
+	String membershipId = String.format(
+
+		"Customer: %s %s %s Membership ID is: %s, enjoy your %d%% discount starting today!\n", firstName,
+		middleInitial, lastName, emailAddress, mailingAddress, phoneNumber, UUID.randomUUID().toString(),
+		StoreConstants.TODAYS_MEMBER_DISCOUNT);
+	return membershipId;
+    }
 
 }
