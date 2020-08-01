a74d02cb251d19e071943397801c755ee9a32c5f
allmajohnson <65092128+allmajohnson@users.noreply.github.com>
2020-08-01 08:41:00 -0700

    Created Store Check Out
    Created Store Check Out Class to extend Receipt Class.

--- projects/src/main/java/com/usi/src/smartcart/StoreCheckOut.java
+++ projects/src/main/java/com/usi/src/smartcart/StoreCheckOut.java
@@ -1,28 +1,18 @@
-/**
- * Property of Urban Survival Initiative
- * Copyright (c) Notice 2020 - All rights reserved
- * 
- * This software is owned in whole by the Urban Survival Initiative.  It's intent purpose
- * is for training individuals to be software engineers.
- * 
- */
 package smartcart;
-
-import java.util.List;
-
-import util.Product;
 
 /**
  * @author Allma M. Johnson
  *
  */
+
 public class StoreCheckOut extends Receipt {
 
     /**
      * @param prodCheckout
      */
+
     public StoreCheckOut() {
-	
+
 	// TODO Auto-generated constructor stub
     }
 
