bb4b4f292ff62c4bad9ec4ccd3a54ed49d7d0056
Ishan Dan Birchett, Sr <ibirchettsr@gmail.com>
2020-08-11 22:19:22 -0400

    Page Layout Dimensions
    Had to comment out some code in Receipt I added to find CENTER.

--- projects/src/main/java/com/usi/src/smartcart/Receipt.java
+++ projects/src/main/java/com/usi/src/smartcart/Receipt.java
@@ -18,6 +18,10 @@ import java.util.Map;
 import java.util.Properties;
 
 import customerservice.Customer;
+import javafx.print.PageLayout;
+import javafx.print.PageOrientation;
+import javafx.print.Paper;
+import javafx.print.Printer;
 import util.Product;
 import util.StoreConstants;
 import util.StorePrinter;
@@ -30,113 +34,120 @@ import util.StorePrinter;
 
 public class Receipt extends StorePrinter {
 
-	Customer cust = new Customer();
-	private String membershipId;
-	String member = membershipId;
-	List<Product> prods = null;
-	private static int counter = 0;
+    Customer cust = new Customer();
+    private String membershipId;
+    String member = membershipId;
+    List<Product> prods = null;
+    private static int counter = 0;
 
-	@Override
-	public int print(Graphics g, PageFormat pf, int page) throws PrinterException {
-		int xSpace = 50;
-		int ySpace = 30;
+    @Override
+    public int print(Graphics g, PageFormat pf, int page) throws PrinterException {
+	int xSpace = 50;
+	int ySpace = 30;
 
-		if (page > 0) { /* We have only one page, and 'page' is zero-based */
-			return NO_SUCH_PAGE;
-		}
-
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.translate(pf.getImageableX(), pf.getImageableY());
-
-		Map<TextAttribute, Integer> fontAttributes = new HashMap<TextAttribute, Integer>();
-		fontAttributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
-
-		/* Now we perform our rendering */
-		Font newFont = new Font(Font.SANS_SERIF, Font.BOLD, 16).deriveFont(fontAttributes);
-		g.setFont(newFont);
-		String storeName = StoreConstants.STORE_NAME;
-		g.drawString(storeName, xSpace, ySpace);
-
-		Date curDate = new Date();
-		String todaysDate = DateFormat.getDateTimeInstance().format(curDate);
-		String datePurchased = String.format("Date: %s", todaysDate);
-		newFont = new Font(Font.SANS_SERIF, Font.PLAIN, 12);
-		g.setFont(newFont);
-		// Next line
-		ySpace += 20;
-		g.drawString(datePurchased, xSpace, ySpace);
-
-		double total = 0.00;
-		// For each loop
-		ySpace += 20;
-		for (Product prod : prods) {
-			String list = String.format("Product: %s, Quantity: %s, Price: %.2f\n", prod.getProductName(),
-					prod.getQuantity(), prod.getPrice());
-			total += prod.getPrice();
-			g.drawString(list, xSpace, ySpace);
-			ySpace += 15;
-		}
-		double totalAmount = total;
-		String tt = String.format("Your total today is $%.2f", totalAmount);
-		ySpace += 5;
-		g.drawString(tt, xSpace, ySpace);
-		/* tell the caller that this page is part of the printed document */
-		return PAGE_EXISTS;
+	if (page > 0) { /* We have only one page, and 'page' is zero-based */
+	    return NO_SUCH_PAGE;
 	}
 
-	public void printReceipt() {
-		System.out.printf("Here is your receipt. Thank you for shopping at the %s\n today!", StoreConstants.STORE_NAME);
-		PrinterJob job = PrinterJob.getPrinterJob();
-		job.setPrintable(this);
-		boolean ok = job.printDialog();
-		if (ok) {
-			try {
-				job.print();
+	Graphics2D g2d = (Graphics2D) g;
+	g2d.translate(pf.getImageableX(), pf.getImageableY());
 
-				System.out.printf("Number of copies: %d", job.getCopies());
-			} catch (PrinterException ex) {
-				System.out.println("The job did not successfully print");
-			}
-		}
+	Map<TextAttribute, Integer> fontAttributes = new HashMap<TextAttribute, Integer>();
+	fontAttributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+
+	/* Now we perform our rendering */
+	Font newFont = new Font(Font.SANS_SERIF, Font.BOLD, 16).deriveFont(fontAttributes);
+	g.setFont(newFont);
+	String storeName = StoreConstants.STORE_NAME;
+	g.drawString(storeName, xSpace, ySpace);
+
+	Date curDate = new Date();
+	String todaysDate = DateFormat.getDateTimeInstance().format(curDate);
+	String datePurchased = String.format("Date: %s", todaysDate);
+	newFont = new Font(Font.SANS_SERIF, Font.PLAIN, 12);
+	g.setFont(newFont);
+	// Next line
+	ySpace += 20;
+	g.drawString(datePurchased, xSpace, ySpace);
+
+	double total = 0.00;
+	// For each loop
+	ySpace += 20;
+	for (Product prod : prods) {
+	    String list = String.format("Product: %s, Quantity: %s, Price: %.2f\n", prod.getProductName(),
+		    prod.getQuantity(), prod.getPrice());
+	    total += prod.getPrice();
+	    g.drawString(list, xSpace, ySpace);
+	    ySpace += 15;
 	}
+	double totalAmount = total;
+	String tt = String.format("Your total today is $%.2f", totalAmount);
+	ySpace += 5;
+	g.drawString(tt, xSpace, ySpace);
+	/* tell the caller that this page is part of the printed document */
+	return PAGE_EXISTS;
+    }
 
-	public static void emailReceipt() {
-		// Prototype
-		String key = "membershipId";
-		String value = "member's email";
-		Properties properties = new Properties();
-		properties.put(key, value);
-		properties.put("mail.smtp.auth", true);
-		properties.put("mail.smtp.starttls.enable", true);
-		properties.put("mail.smtp.host", "smtp.gmail.com");
-		properties.put("mail.smtp.port", "587");
+    public void printReceipt() {
+	System.out.printf("Here is your receipt. Thank you for shopping at the %s\n today!", StoreConstants.STORE_NAME);
+	Printer printer = Printer.getDefaultPrinter();
+	PageLayout pageLayout = printer.createPageLayout(Paper.NA_LETTER, PageOrientation.PORTRAIT,
+		Printer.MarginType.DEFAULT);
+//	        double scaleX = pageLayout.getPrintableWidth() / node.getBoundsInParent().getWidth();
+//	        double scaleY = pageLayout.getPrintableHeight() / node.getBoundsInParent().getHeight();
+//	        node.getTransforms().add(new Scale(scaleX, scaleY));
 
-		String MyAccountEmail = "superstore0502@gmail.com";
-		String password = "superstore0502";
+	PrinterJob job = PrinterJob.getPrinterJob();
+	job.setPrintable(this);
+	boolean ok = job.printDialog();
+	if (ok) {
+	    try {
+		job.print();
 
-		System.out.printf("Your reciept will be emailed to you. Thank you for shopping at the %s\\n today!",
-				StoreConstants.STORE_NAME);
-
+		System.out.printf("Number of copies: %d", job.getCopies());
+	    } catch (PrinterException ex) {
+		System.out.println("The job did not successfully print");
+	    }
 	}
+    }
 
-	public static void member() {
-		System.out.printf("You saved $%.2f today!");
-	}
+    public static void emailReceipt() {
+	// Prototype
+	String key = "membershipId";
+	String value = "member's email";
+	Properties properties = new Properties();
+	properties.put(key, value);
+	properties.put("mail.smtp.auth", true);
+	properties.put("mail.smtp.starttls.enable", true);
+	properties.put("mail.smtp.host", "smtp.gmail.com");
+	properties.put("mail.smtp.port", "587");
 
-	public static void nonMember() {
-		System.out.printf("You could have saved $%.2f today!");
-	}
+	String MyAccountEmail = "superstore0502@gmail.com";
+	String password = "superstore0502";
 
-	/**
-	 * 
-	 */
-	public Receipt() {
+	System.out.printf("Your reciept will be emailed to you. Thank you for shopping at the %s\\n today!",
+		StoreConstants.STORE_NAME);
 
-		super();
-	}
+    }
 
-	public void setProdList(List<Product> prodList) {
-		prods = prodList;
-	}
+    public static void member() {
+	System.out.printf("You saved $%.2f today!");
+    }
+
+    public static void nonMember() {
+	System.out.printf("You could have saved $%.2f today!");
+    }
+
+    /**
+     * 
+     */
+    public Receipt() {
+
+	super();
+    }
+
+    public void setProdList(List<Product> prodList) {
+	prods = prodList;
+    }
 
 }
