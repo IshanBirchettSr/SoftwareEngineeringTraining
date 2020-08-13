8731a9e9b701bb0c22de0440a66c399a6161c1a9
Charlene Cornelius <chichiwepe@aol.com>
2020-08-13 03:36:04 -0400

    .

--- projects/src/main/java/com/usi/src/electronics/ElectronicsDept.java
+++ projects/src/main/java/com/usi/src/electronics/ElectronicsDept.java
@@ -9,6 +9,7 @@
 package electronics;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
@@ -20,13 +21,18 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import util.DataCsvLoad;
 import util.Department;
@@ -89,11 +95,11 @@ public class ElectronicsDept extends Department {
 	@Override
 	public void listProducts() {
 		String aKey = null;
-		Set<String> electronicProductKeys = electronicsProducts.keySet();
+		Set<String> eProductKeys = electronicsProducts.keySet();
 
-		int totalProducts = electronicProductKeys.size();
+		int totalProducts = eProductKeys.size();
 		int i = 1;
-		for (String pKey : electronicProductKeys) {
+		for (String pKey : eProductKeys) {
 			Product pd = electronicsProducts.get(pKey);
 			if (aKey != pKey) {
 				System.out.printf("%d: %s %s\t%.2f\n", i, pd.getBrandName(), pd.getProductName(), pd.getPrice());
@@ -188,7 +194,109 @@ public class ElectronicsDept extends Department {
 
 		eBox.getChildren().add(paneCharacter);
 
-		Scene eScene = new Scene(eBox, 600, 575);
+		Label instructions = new Label("Hover mouse over image for Brand, Product and Price Info.");
+		instructions.setAlignment(Pos.CENTER);
+		instructions.setFont(Font.font("Rockwell", FontWeight.BOLD, FontPosture.ITALIC, 16));
+		instructions.setStyle("-fx-background-color:lightblue");
+		VBox epr = new VBox(15, eBox, iv, instructions);
+		epr.setAlignment(Pos.CENTER);
+
+		// Product Grid
+		GridPane pGrid = new GridPane();
+		Insets iSet = new Insets(0, 30, 10, 10);
+		pGrid.setPadding(iSet);
+
+		String oProdName = "NoProd";
+		Set<String> eProductKeys = electronicsProducts.keySet();
+		// You must sort the Set of keys
+		List<String> list = new ArrayList<>(eProductKeys);
+		Collections.sort(list);
+
+		int rowIndex = 0;
+		int columnIndex = 0;
+		String oldFilename = "Firstfile";
+
+		for (String pKey : list) {
+			Product pd = electronicsProducts.get(pKey);
+
+			String iFileName = String.format(StoreConstants.PRODUCT_IMAGE, "electronics", pd.getBrandName(),
+					pd.getProductName());
+			if (oldFilename.equals(iFileName)) {
+				// System.out.printf("%s==%s, %b\n", oldFilename,
+				// iFileName,oldFilename.equals(iFileName));
+				continue;
+			}
+			System.out.println(iFileName);
+			oldFilename = iFileName;
+
+			// Image View
+			Image pImage = new Image(iFileName);
+			ImageView pV = new ImageView();
+			pV.setFitHeight(125);
+			// pV.setFitHeight(65);
+			pV.setId(pd.getBrandName() + "-" + pd.getProductName());
+			pV.setImage(pImage);
+			pV.setPreserveRatio(true);
+
+			pV.setSmooth(true);
+			pV.setCache(true);
+			String electronicsToolTip = String.format("%s - %s $%.2f", pd.getProductName(), pd.getBrandName(),
+					pd.getPrice());
+			Tooltip.install(pV, new Tooltip(electronicsToolTip));
+
+			EventHandler<MouseEvent> iEvent = new EventHandler<MouseEvent>() {
+				public void handle(MouseEvent e) {
+					System.out.printf("Image Click on %s\n", pV.getId());
+				}
+			};
+			pV.setOnMouseClicked(iEvent);
+			if (oProdName.equals(pd.getProductName()) != true) {
+				Label pLabel = new Label();
+				pLabel.setFont(Font.font("Rockwell", FontWeight.BOLD, FontPosture.ITALIC, 30));
+				pLabel.setStyle("-fx-border-color:black; -fx-background-color:gray;");
+				if (pd.getProductName().contains("Ipad")) {
+					pLabel.setText(pd.getProductName() + " Aisle");
+					pLabel.setStyle("-fx-border-color:black; -fx-background-color:gray;");
+				} else {
+					pLabel.setText(pd.getProductName() + " Shelve");
+				}
+				pLabel.setAlignment(Pos.CENTER);
+				columnIndex = 0;
+				rowIndex += 1;
+				System.out.printf("Label: Column: %d, Row: %d\n", columnIndex, rowIndex);
+
+				pGrid.add(pLabel, columnIndex, rowIndex, 10, 1);
+				if (rowIndex == 0) {
+					rowIndex = 1;
+				} else {
+					rowIndex += 1;
+				}
+				System.out.printf("%s vs %s\n", oProdName, pd.getProductName());
+				oProdName = pd.getProductName();
+			}
+			System.out.printf("C-%d, R-%d\n", columnIndex, rowIndex);
+			pGrid.add(pV, columnIndex, rowIndex);
+
+			if (columnIndex < 5) {
+				columnIndex++;
+			} else {
+				rowIndex += 2;
+				columnIndex = 0;
+			}
+
+		}
+		// ap.getChildren().add(pGrid);
+		pGrid.setHgap(20);
+		pGrid.setVgap(40);
+		ScrollPane sp = new ScrollPane();
+		sp.setContent(pGrid);
+		sp.setPannable(true);
+		sp.setHvalue(0.0);
+		sp.setVvalue(0.0);
+
+		VBox eVBox = new VBox(20, epr, sp);
+
+		Scene eScene = new Scene(eVBox, 600, 650);
 
 		return eScene;
 	}
