f758e8cdaecd23dfff40342a96c67453d6e268ce
Ishan Dan Birchett, Sr <ibirchettsr@gmail.com>
2020-08-17 00:03:43 -0400

    Bedding Help
    -- moved copied code to getScene()
    -- removed errors.

--- projects/src/main/java/com/usi/src/bedding/BeddingDept.java
+++ projects/src/main/java/com/usi/src/bedding/BeddingDept.java
@@ -42,7 +42,6 @@ public class BeddingDept extends Department {
     String deptName = StoreConstants.deptNames.BEDDING.name();
     List<String> beddingRecords = null;
     HashMap<Integer, String> keyMap = null;
-// HashMap<K, V> to hold ElectronicsProd objects.
     HashMap<String, BeddingProd> BeddingProducts;
 
     /**
@@ -50,22 +49,18 @@ public class BeddingDept extends Department {
      */
     public BeddingDept() {
 	super.setDeptName(deptName);
-// Record Load
 	DataCsvLoad unLoadTrucks = new DataCsvLoad();
 	unLoadTrucks.loadData(StoreConstants.BEDDING_TRUCK);
 	beddingRecords = unLoadTrucks.getRecords();
 	this.setLoadedRecords(beddingRecords);
-// System.out.printf("%s Department open with %d records\n", deptName,
-// autoRecords.size());
 	keyMap = new HashMap<Integer, String>();
-// Automotive Product Load
 	BeddingProducts = new HashMap<String, BeddingProd>();
 	loadProducts();
     }
 
     @Override
     protected void loadProducts() {
-// Load products
+	// Load products
 	for (String record : beddingRecords) {
 	    BeddingProd bp = new BeddingProd();
 	    boolean recordToProductSuccessful = bp.recordToProduct(record);
@@ -87,8 +82,6 @@ public class BeddingDept extends Department {
     public void listProducts() {
 	String aKey = null;
 	Set<String> aProductKeys = BeddingProducts.keySet();
-
-	int totalProducts = aProductKeys.size();
 	int i = 1;
 	for (String pKey : aProductKeys) {
 	    Product pd = BeddingProducts.get(pKey);
@@ -119,10 +112,13 @@ public class BeddingDept extends Department {
 	return pList;
     }
 
-    String sString = String.format("We have all your %s needs!", StoreConstants.deptNames.BEDDING);
+    @Override
+    public Scene getScene() {
+
+	String sString = String.format("We have all your %s needs!", StoreConstants.deptNames.BEDDING);
 	Label slogan = new Label();
-	slogan.@setText(sString)
-	slogan.setAlignment(Pos.CENTER)
+	slogan.setText(sString);
+	slogan.setAlignment(Pos.CENTER);
 	slogan.setTextFill(Color.BLUE);
 	slogan.setFont(Font.font("Verdana", FontPosture.REGULAR, 20));
 	Image beddingImage = new Image(StoreConstants.BEDDINGDEPT);
@@ -137,12 +133,12 @@ public class BeddingDept extends Department {
 	instructions.setFont(Font.font("Rockwell", FontWeight.BOLD, FontPosture.ITALIC, 16));
 	instructions.setStyle("-fx-background-color:lightblue");
 	VBox ap = new VBox(15, slogan, iv, instructions);
-	ap.setAlignment(Pos.CENTER)
+	ap.setAlignment(Pos.CENTER);
 
 	// Product Grid
 	GridPane pGrid = new GridPane();
 	Insets iSet = new Insets(0, 30, 10, 10);
-	pGrid.setPadding(iSet)
+	pGrid.setPadding(iSet);
 	String oProdName = "NoProd";
 	Set<String> aProductKeys = BeddingProducts.keySet();
 	// You must sort the Set of keys
@@ -151,96 +147,96 @@ public class BeddingDept extends Department {
 
 	int rowIndex = 0;
 	int columnIndex = 0;
-	String oldFilename = "Firstfile";{
+	String oldFilename = "Firstfile";
+	{
 
-	for (String pKey : list) {
-	    Product pd = BeddingProducts.get(pKey);
+	    for (String pKey : list) {
+		Product pd = BeddingProducts.get(pKey);
 
-	    String iFileName = String.format(StoreConstants.PRODUCT_IMAGE, "bedding", pd.getBrandName(),
-		    pd.getProductName());
-	    if (oldFilename.equals(iFileName)) {
-		// System.out.printf("%s==%s, %b\n", oldFilename,
-		// iFileName,oldFilename.equals(iFileName));
-		continue;
-	    }
-	    System.out.println(iFileName);
-	    oldFilename = iFileName;
-
-	    // Image View
-	    Image pImage = new Image(iFileName);
-	    ImageView pV = new ImageView();
-	    pV.setFitHeight(125);
-	    // pV.setFitHeight(65);
-	    pV.setId(pKey);
-	    pV.setImage(pImage);
-	    pV.setPreserveRatio(true);
-
-	    pV.setSmooth(true);
-	    pV.setCache(true);
-	    String beddingToolTip = String.format("%s - %s $%.2f", pd.getProductName(), pd.getBrandName(), pd.getPrice());
-	    Tooltip.install(pV, new Tooltip(beddingToolTip));
-
-	    EventHandler<MouseEvent> iEvent = new EventHandler<MouseEvent>() {
-		public void handle(MouseEvent e) {
-		    System.out.printf("Image Click on %s\n", pV.getId());
-		    Product pd2 = BeddingProducts.get(pKey);
-		    Greeting.prodDetails(pd2, "auto");
-
+		String iFileName = String.format(StoreConstants.PRODUCT_IMAGE, "bedding", pd.getBrandName(),
+			pd.getProductName());
+		if (oldFilename.equals(iFileName)) {
+		    // System.out.printf("%s==%s, %b\n", oldFilename,
+		    // iFileName,oldFilename.equals(iFileName));
+		    continue;
 		}
-	    };
-	    pV.setOnMouseClicked(iEvent);
-	    if (oProdName.equals(pd.getProductName()) != true) {
-		Label pLabel = new Label();
-		pLabel.setFont(Font.font("Rockwell", FontWeight.BOLD, FontPosture.ITALIC, 30));
-		pLabel.setStyle("-fx-border-color:black; -fx-background-color:yellow;");
-		if (pd.getProductName().contains("Tire")) {
-		    pLabel.setText(pd.getProductName() + " Aisle");
-		    pLabel.setStyle("-fx-border-color:black; -fx-background-color:orange;");
-		} else {
-		    pLabel.setText(pd.getProductName() + " Shelve");
-		}
-		pLabel.setAlignment(Pos.CENTER);
-		columnIndex = 0;
-		rowIndex += 1;
-		// System.out.printf("Label: Column: %d, Row: %d\n", columnIndex, rowIndex);
+		System.out.println(iFileName);
+		oldFilename = iFileName;
 
-		pGrid.add(pLabel, columnIndex, rowIndex, 10, 1);
-		if (rowIndex == 0) {
-		    rowIndex = 1;
-		} else {
+		// Image View
+		Image pImage = new Image(iFileName);
+		ImageView pV = new ImageView();
+		pV.setFitHeight(125);
+		// pV.setFitHeight(65);
+		pV.setId(pKey);
+		pV.setImage(pImage);
+		pV.setPreserveRatio(true);
+
+		pV.setSmooth(true);
+		pV.setCache(true);
+		String beddingToolTip = String.format("%s - %s $%.2f", pd.getProductName(), pd.getBrandName(),
+			pd.getPrice());
+		Tooltip.install(pV, new Tooltip(beddingToolTip));
+
+		EventHandler<MouseEvent> iEvent = new EventHandler<MouseEvent>() {
+		    public void handle(MouseEvent e) {
+			System.out.printf("Image Click on %s\n", pV.getId());
+			Product pd2 = BeddingProducts.get(pKey);
+			Greeting.prodDetails(pd2, "auto");
+
+		    }
+		};
+		pV.setOnMouseClicked(iEvent);
+		if (oProdName.equals(pd.getProductName()) != true) {
+		    Label pLabel = new Label();
+		    pLabel.setFont(Font.font("Rockwell", FontWeight.BOLD, FontPosture.ITALIC, 30));
+		    pLabel.setStyle("-fx-border-color:black; -fx-background-color:yellow;");
+		    if (pd.getProductName().contains("Tire")) {
+			pLabel.setText(pd.getProductName() + " Aisle");
+			pLabel.setStyle("-fx-border-color:black; -fx-background-color:orange;");
+		    } else {
+			pLabel.setText(pd.getProductName() + " Shelve");
+		    }
+		    pLabel.setAlignment(Pos.CENTER);
+		    columnIndex = 0;
 		    rowIndex += 1;
+		    // System.out.printf("Label: Column: %d, Row: %d\n", columnIndex, rowIndex);
+
+		    pGrid.add(pLabel, columnIndex, rowIndex, 10, 1);
+		    if (rowIndex == 0) {
+			rowIndex = 1;
+		    } else {
+			rowIndex += 1;
+		    }
+		    // System.out.printf("%s vs %s\n", oProdName, pd.getProductName());
+		    oProdName = pd.getProductName();
 		}
-		// System.out.printf("%s vs %s\n", oProdName, pd.getProductName());
-		oProdName = pd.getProductName();
-	    }
-	    // System.out.printf("C-%d, R-%d\n", columnIndex, rowIndex);
-	    pGrid.add(pV, columnIndex, rowIndex);
+		// System.out.printf("C-%d, R-%d\n", columnIndex, rowIndex);
+		pGrid.add(pV, columnIndex, rowIndex);
 
-	    if (columnIndex < 5) {
-		columnIndex++;
-	    } else {
-		rowIndex += 2;
-		columnIndex = 0;
-	    }
+		if (columnIndex < 5) {
+		    columnIndex++;
+		} else {
+		    rowIndex += 2;
+		    columnIndex = 0;
+		}
 
+	    }
+	    // ap.getChildren().add(pGrid);
+	    pGrid.setHgap(20);
+	    pGrid.setVgap(40);
+	    ScrollPane sp = new ScrollPane();
+	    sp.setContent(pGrid);
+	    sp.setPannable(true);
+	    sp.setHvalue(0.0);
+	    sp.setVvalue(0.0);
+	    HBox dButtons = Greeting.getBottonDeptButtons();
+	    dButtons.setAlignment(Pos.CENTER);
+	    dButtons.setSpacing(30);
+	    dButtons.setPadding(new Insets(15, 0, 15, 0));
+	    VBox aVBox = new VBox(10, ap, sp, dButtons);
+	    Scene aScene = new Scene(aVBox, 400, 650);
+	    return aScene;
 	}
-	// ap.getChildren().add(pGrid);
-	pGrid.setHgap(20);
-	pGrid.setVgap(40);
-	ScrollPane sp = new ScrollPane();
-	sp.setContent(pGrid);
-	sp.setPannable(true);
-	sp.setHvalue(0.0);
-	sp.setVvalue(0.0);
-	HBox dButtons = Greeting.getBottonDeptButtons();
-	dButtons.setAlignment(Pos.CENTER);
-	dButtons.setSpacing(30);
-	dButtons.setPadding(new Insets(15, 0, 15, 0));
-	VBox aVBox = new VBox(10, ap, sp, dButtons);
-	Scene aScene = new Scene(aVBox, 400, 650);
-	return aScene;
-
-	// TODO Auto-generated method stub
-	return beddScene;
     }
 }
