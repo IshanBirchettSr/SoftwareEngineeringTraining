24b59e97d8d5c352cac5b63e3fc0020e27f47049
Charlene Cornelius <chichiwepe@aol.com>
2020-10-13 17:10:06 -0400

    MySQL records loaded
    Loaded data from for dept e-m trucks.

--- projects/src/main/java/com/usi/src/resources/SQL/FURNITURE_TRUCK_LOAD.sql
+++ projects/src/main/java/com/usi/src/resources/SQL/FURNITURE_TRUCK_LOAD.sql
@@ -1,7 +1,7 @@
-LOAD DATA INFILE 'C:\\usi-git\\SoftwareEngineeringTraining\\projects\\src\\main\\java\\com\\usi\\src\\resources\\Electronics.txt'
-INTO TABLE ELECTRONICS
+LOAD DATA INFILE 'C:\\usi-git\\SoftwareEngineeringTraining\\projects\\src\\main\\java\\com\\usi\\src\\resources\\Furniture.txt'
+INTO TABLE FURNITURE
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-IGNORE 2 ROWS
+IGNORE 4 ROWS
 (PRODUCT_NAME , DEPT_NAME , UNIT_OF_MEASURE , BARCODE , QUANTITY , SIZE , BRAND_NAME , WEIGHT , PRICE , COLOR , INGREDIENT , NUMBER_UNITS_IN_STOCK , DESCRIPTION_OF_PRODUCT);
