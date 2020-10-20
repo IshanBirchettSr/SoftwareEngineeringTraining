e1ee3913b17ef217cfa16dea006eee44d3b173a1
Charlene Cornelius <chichiwepe@aol.com>
2020-10-20 19:29:50 -0400

    SQL-INSERT-INTO
    LEARNING NEW SEQUEL COMMANDS

---
+++ projects/src/main/java/com/usi/src/resources/SQL/MEMEBERSHIP_INSERTS_EXAMPLE.sql
@@ -0,0 +1,7 @@
+INSERT INTO MEMBERSHIP (first_name, middle_name, last_name, email_address, street_address, city, state, postal_code, phone_number, aarp, date_of_membership)
+VALUES('Martin', 'E', 'Johnson', 'Martin@gmail.com','341 Been around Rd', 'AL', 'Buckeye', '10567', '7186711401', 'NO', '2020-01-01'),
+('Veronica', 'B', 'Wall', 'AngelVee@gmail.com','10 Emprerors Way', 'CT', 'City', '10567', '9172286534', 'YES', '2020-03-2'),
+('Linda', 'P', 'Leesen', 'ListenLindaaa93@gmail.com','61 Valley Green', 'NJ', 'Newark', '10567', '2523669875', 'NO', '2020-04-01'),
+('Sharonda', 'J', 'Betts', 'SharShar83@gmail.com','87 Combo Blvd', 'Oh', 'Ohio', '10567', '3296511478','YES', '2020-05-01'),
+('Gregory', 'K', 'Haynes', 'GregsTheMan01@gmail.com','12 Penn Plaza', 'Ia', 'Idaho', '10567', '5128887923','NO', '2020-06-01'),
+('Sam', 'A', 'Ashford', 'SamAsh54@gmail.com','100 Martin Luther King Blvd', 'NY', 'New York', '10678', '9120562356','NO', '2020-05-05');
